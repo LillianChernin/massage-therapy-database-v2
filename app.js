877092bd01366f1e7d305fb449bb03c1ca25fddd
Lillian Chernin <l.smith1654@gmail.com>
2018-03-10 22:44:37 -0600

    try to enable ajax calls

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ try {
   ENV = process.env;
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
+
 mongoose.connect(ENV.MONGODB_URI);
 
 app.use(bodyParser.json());
